23bd9f093fb548355769ad6c7cceaa76704e2685
Nikhil Thota <nthota92@gmail.com>
2022-12-01 16:04:24 -0500

    we can log into the frontend, which is connect to supabase! supa easy

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import {useState} from "react";
+import {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+                 Component,
+                 pageProps,
+               }: AppProps<{
+  initialSession: Session,
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
+export default MyApp
